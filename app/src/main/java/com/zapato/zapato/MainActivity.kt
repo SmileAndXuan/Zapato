feef09446d679bf2b3133388625e7c853244a468
James Gang <anjin.gang@student.csulb.edu>
2018-02-13 17:20:20 -0800

    SharedPreferences login starter successfully running. Need to clean up.

--- app/src/main/java/com/zapato/zapato/MainActivity.kt
+++ app/src/main/java/com/zapato/zapato/MainActivity.kt
@@ -152,7 +152,7 @@ class MainActivity : AppCompatActivity() {
                         prefs = Prefs(this)
                         val bgColor = prefs!!.bgColor
 
-                        var color = 7777
+                        var color = 777
                         prefs!!.bgColor = color
 
                         Log.d("Store my data", "" + prefs!!.bgColor)
